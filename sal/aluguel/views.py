cecfe6f3d5718436d22f5d46096b46070c9869cc
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-05-17 09:23:46 -0300

    add: numero de livros e instancias disponiveis

--- sal/aluguel/views.py
+++ sal/aluguel/views.py
@@ -6,9 +6,13 @@ from django.http import HttpResponse
 
 def index(request):
 
+  num_livros = models.Livro.objects.all().count()
   num_instancias = models.InstanciaLivro.objects.all().count()
+  num_instancias_disponiveis = models.InstanciaLivro.objects.filter(status__exact='d').count()
 
   context = {
+    'num_livros': num_livros,
     'num_instancias': num_instancias,
+    'num_instancias_disponiveis': num_instancias_disponiveis,
     }
   return render(request, 'index.html', context=context)
