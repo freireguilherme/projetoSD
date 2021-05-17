5846eaa370acad5ff7ddace1f7521b34c3b7680b
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-05-17 04:38:10 -0300

    new func: retorna numero de instancias

--- sal/aluguel/views.py
+++ sal/aluguel/views.py
@@ -1,7 +1,14 @@
+from . import models
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Aqui aparecerá a pagina inicial")
+
+  num_instancias = models.InstanciaLivro.objects.all().count()
+
+  context = {
+    'num_instancias': num_instancias,
+    }
+  return render(request, 'index.html', context=context)
