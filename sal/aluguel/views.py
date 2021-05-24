4faa48874d44a4fb3fdf87d2ddcf0221da01c0b1
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-05-24 10:42:52 -0300

    func: listar livros

--- sal/aluguel/views.py
+++ sal/aluguel/views.py
@@ -9,10 +9,12 @@ def index(request):
   num_livros = models.Livro.objects.all().count()
   num_instancias = models.InstanciaLivro.objects.all().count()
   num_instancias_disponiveis = models.InstanciaLivro.objects.filter(status__exact='d').count()
+  listar_livros = models.Livro.objects.all()
 
   context = {
     'num_livros': num_livros,
     'num_instancias': num_instancias,
     'num_instancias_disponiveis': num_instancias_disponiveis,
+    'listar_livros': listar_livros,
     }
   return render(request, 'index.html', context=context)
