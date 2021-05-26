0afbd76d6dc56ada8b01acbc78dbdd337df164d1
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-05-26 03:08:32 -0300

    add cadastro url e listar livros url

--- sal/aluguel/urls.py
+++ sal/aluguel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import *
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', IndexView.as_view(), name='index'),
+  path('cadastrar/cliente', ClienteCreate.as_view(), name='cadastrar-cliente'),
+  path('listar/livros', LivroList.as_view(), name='listar-livros'),
 ]
