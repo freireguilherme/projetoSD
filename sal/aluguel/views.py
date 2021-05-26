a1b1de1dcee65e9577c2bd8a6c04a8b1ca2d4900
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-05-26 03:09:25 -0300

    add view for cadastro de cliente e listar livros

--- sal/aluguel/views.py
+++ sal/aluguel/views.py
@@ -1,10 +1,27 @@
 from . import models
+from .models import *
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+class IndexView(TemplateView):
+  template_name="base_generic.html"
+class ClienteCreate(CreateView):
+  model = Cliente
+  fields = ['cpf', 'nome', 'email', 'cep', 'bairro', 'senha']
+  template_name = 'formulario.html'
+  success_url = reverse_lazy('index')
 
-def index(request):
+class LivroList(ListView):
+  model = Livro
+  template_name = 'listar_livros.html'
+"""
+  def index(request):
 
   num_livros = models.Livro.objects.all().count()
   num_instancias = models.InstanciaLivro.objects.all().count()
@@ -18,3 +35,4 @@ def index(request):
     'listar_livros': listar_livros,
     }
   return render(request, 'index.html', context=context)
+  """
