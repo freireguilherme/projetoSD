8b4d491f7c4e5616a36021e9e59b35c110f5c117
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-06-03 01:39:24 -0300

    add link to cadastrar livro

--- sal/aluguel/urls.py
+++ sal/aluguel/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
   path('', IndexView.as_view(), name='index'),
   path('cadastrar/cliente', ClienteCreate.as_view(), name='cadastrar-cliente'),
+  path('cadastrar/livro', LivroCreate.as_view(), name='cadastrar-livro'),
   path('listar/livros', LivroList.as_view(), name='listar-livros'),
 ]
