6b865e2119ef7c7fff24c56a171000cce89057cc
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-06-04 03:42:56 -0300

    add alterar senha path

--- sal/usuarios/urls.py
+++ sal/usuarios/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
     template_name='usuarios/login.html',
     extra_context={'titulo': 'Autenticação'}
 ), name='login'),
+
+path('alterar-minha-senha/', auth_views.PasswordChangeView.as_view(
+        template_name='usuarios/login.html',
+        extra_context={'titulo': 'Alterar senha atual'},
+        success_url=reverse_lazy('index')
+        ), name="alterar_senha"),
+
 path('sair/', auth_views.LogoutView.as_view(), name="logout"),
 ]
