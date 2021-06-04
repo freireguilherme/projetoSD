af5d7c94ab8748a9b6141972b1a6fed7fdda61b2
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-06-03 22:49:05 -0300

    add link para editar e excluir

--- sal/aluguel/urls.py
+++ sal/aluguel/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
 from .views import *
 
 urlpatterns = [
   path('', IndexView.as_view(), name='index'),
   path('cadastrar/cliente', ClienteCreate.as_view(), name='cadastrar-cliente'),
   path('cadastrar/livro', LivroCreate.as_view(), name='cadastrar-livro'),
+  path('editar/livro/<int:pk>/', LivroUpdate.as_view(), name='editar-livro'),
+  path('excluir/livro/<int:pk>/', LivroDelete.as_view(), name='excluir-livro'),
   path('listar/livros', LivroList.as_view(), name='listar-livros'),
 ]
