a48db455b2cc4e360fdb6e326a0581c86ab0c9e4
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-06-03 23:20:19 -0300

    add class Cliente

--- sal/usuarios/models.py
+++ sal/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+  cpf = models.IntegerField()
+  nome = models.CharField(max_length=200)
+  senha = models.CharField(max_length=8)
+  email = models.EmailField()
+  cep = models.IntegerField()
+  rua = models.CharField(max_length=200)
+  bairro = models.CharField(max_length=200)
+  
+  def __str__(self):
+    return self.nome
