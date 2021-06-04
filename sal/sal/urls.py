57287aeec3c56599e0770bf4fd8ccad4f28b6db0
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2021-06-03 23:38:10 -0300

    add login url

--- sal/sal/urls.py
+++ sal/sal/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('aluguel/', include('aluguel.urls')),
+    path('', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
